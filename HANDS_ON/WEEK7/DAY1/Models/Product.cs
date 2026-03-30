605ed16e4e3cbdbb118f5a9586bb5296ce9056b7
hemanth Bhupalam <bhupalamhemanth1042@gmail.com>
2026-03-30 22:31:18 +0530

    week7 day1 views added

---
+++ HANDS_ON/WEEK7/DAY1/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models
+{
+    public class Product
+    {
+        [Required(ErrorMessage= "Product Id is required")]
+        public int Id { get; set; }
+        [Required(ErrorMessage ="Product Name is required")]
+        [StringLength(15,MinimumLength =5,ErrorMessage ="Name must be 5 to 15 characters")]
+        public string? Name { get; set; }
+        [Required(ErrorMessage ="Price is required")]
+        public decimal Price { get; set; }
+        [Required(ErrorMessage = "Category is required")]
+        [StringLength(15,MinimumLength =5,ErrorMessage ="Category must be 5 to 15 characters")]
+        public string? Category { get; set; }
+    }
+}
